b51cb5a2e9eb58d95662413af8e1b6b27db8b435
JuanFe <j_felipe05@hotmail.com>
2025-11-18 13:55:01 -0500

    se organizan los routers

--- backend/app/presentation/router/index.py
+++ backend/app/presentation/router/index.py
@@ -6,6 +6,6 @@ from app.presentation.controller.role_controller import router as role_router
 
 router = APIRouter()
 
-router.include_router(auth_router, prefix="/auth", tags=["Auth"])
-router.include_router(user_router, prefix="/users", tags=["Users"])
-router.include_router(role_router, prefix="/roles", tags=["Roles"])
+router.include_router(auth_router, tags=["Auth"])
+router.include_router(user_router, tags=["Users"])
+router.include_router(role_router, tags=["Roles"])
