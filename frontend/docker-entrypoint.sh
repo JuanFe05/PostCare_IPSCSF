13eb8bd61994cbd008cb5f81b9306b12b725c680
ipscsf <estadisticaipssaludflorida@gmail.com>
2026-01-05 14:17:38 -0500

    se arregla un bug en el entrypoint del frontend

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Generar config.js dinámicamente con la URL del backend desde variable de entorno
-cat > /usr/share/nginx/html/config.js << EOF
+cat > /usr/share/nginx/html/config.js << 'EOF'
 window.APP_CONFIG = {
   BACKEND_URL: '${VITE_BACKEND_URL:-http://localhost:48555}'
 };
